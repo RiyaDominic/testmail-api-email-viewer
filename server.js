1a147a77206a00932494cbf8c1db2716653c0596
Riya Dominic <riyadominic2002@gmail.com>
2026-09-05 21:40:10 +0530

    fix: prepare backend for deployment

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.use((req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
